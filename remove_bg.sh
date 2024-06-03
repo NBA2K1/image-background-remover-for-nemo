1b20101c825893f89eda0a293278847a88052b28
NBA2K1 <78034913+NBA2K1@users.noreply.github.com>
2024-06-03 05:03:53 +0200

    Now supports spaces in the filepath

--- remove_bg.sh
+++ remove_bg.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-rembg i $1  "${3}/bgremoved-${2}"
-notify-send "Operation Completed"
+rembg i "$1" "$3/bgremoved-${2}"
+
+if [ $? -eq 0 ]; then
+	notify-send "Operation completed successfully"
+else
+	notify-send "Operation wasn't successful. An Error ocurred"
+fi

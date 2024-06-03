a4fad622807ae1b54aace27daf4906ba243f67b7
NBA2K1 <78034913+NBA2K1@users.noreply.github.com>
2024-06-03 07:09:49 +0200

    Update remove_bg.sh
    To make it compatible with the install script

--- remove_bg.sh
+++ remove_bg.sh
@@ -2,7 +2,7 @@
 rembg i "$1" "$3/bgremoved-${2}"
 
 if [ $? -eq 0 ]; then
-	notify-send "Operation completed successfully"
+	notify-send "SUCCESS"
 else
-	notify-send "Operation wasn't successful. An Error ocurred"
+	notify-send "ERROR"
 fi
